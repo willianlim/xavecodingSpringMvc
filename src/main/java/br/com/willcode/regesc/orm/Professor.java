1b6a8d5ad5a2de5b6298313f926610f3131eb02c
willianlim <willianrosendodelima@outlook.com>
2022-12-06 22:52:20 -0300

    creating the teacher table

---
+++ src/main/java/br/com/willcode/regesc/orm/Professor.java
@@ -0,0 +1,17 @@
+package br.com.willcode.regesc.orm;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String prontuario;
+
+}
