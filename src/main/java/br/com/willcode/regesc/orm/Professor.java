cda776e6dcb3163f52a8330105bfa4d12ececb4a
willianlim <willianrosendodelima@outlook.com>
2022-12-08 00:34:40 -0300

    updating a database record

--- src/main/java/br/com/willcode/regesc/orm/Professor.java
+++ src/main/java/br/com/willcode/regesc/orm/Professor.java
@@ -25,6 +25,10 @@ public class Professor {
         this.prontuario = prontuario;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/br/com/willcode/regesc/service/CrudProfessorService.java
+++ src/main/java/br/com/willcode/regesc/service/CrudProfessorService.java
@@ -4,6 +4,7 @@ import br.com.willcode.regesc.orm.Professor;
 import br.com.willcode.regesc.repository.ProfessorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 @Service
@@ -27,13 +28,17 @@ public class CrudProfessorService {
         while (isTrue) {
             System.out.println("\nQual ação você quer executar?");
             System.out.println("0 - Voltar ao menu anterior");
-            System.out.println("1 - Cadastrar novo professor");
+            System.out.println("1 - Cadastrar novo Professor");
+            System.out.println("2 - Atualizar um Professor");
 
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     this.cadastrar(scanner);
+                    break;
+                case 2:
+                    this.atualizar(scanner);
                     break;
                 default:
                     isTrue = false;
@@ -47,12 +52,57 @@ public class CrudProfessorService {
         System.out.println("Digite o nome do professor: ");
         String nome = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
 
-        System.out.println("Digite o protuário do professor: ");
+        System.out.println("Digite o prontuário do professor: ");
         String prontuario = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
 
         Professor professor = new Professor(nome, prontuario);
         this.professorRepository.save(professor);
         System.out.println("Professor salvo no banco!!\n");
+    }
+
+    private void atualizar(Scanner scanner) {
+        System.out.print("Digite o id do Professor a ser atualizado: ");
+        Long id = scanner.nextLong();
+
+        Optional<Professor> optional = this.professorRepository.findById(id);
+
+        if (optional.isPresent()) {
+            System.out.println("Digite o nome do professor: ");
+            String nome = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
+
+            System.out.println("Digite o prontuário do professor: ");
+            String prontuario = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
+
+            Professor professor = optional.get();
+            professor.setNome(nome);
+            professor.setProntuario(prontuario);
+
+            professorRepository.save(professor); // atualiza (persiste) o objecto/registro/tupla no BD
+            System.out.println("Professor atualizado com sucesso !!!\n");
+
+        } else {
+            System.out.println("O id do professor informado: " + id + " é inválido\n");
+        }
+    }
+
+    private void atualizarSerFindById(Scanner scanner) {
+        System.out.println("Digite o Id do Professor a ser atualizado: ");
+        Long id = scanner.nextLong();
+
+        System.out.println("Digite o nome do professor: ");
+        String nome = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
+
+        System.out.println("Digite o prontuário do professor: ");
+        String prontuario = scanner.next(); // Ler a próxima string até achar um enter ou um espaço
+
+        Professor professor = new Professor();
+        professor.setId(id);
+        professor.setNome(nome);
+        professor.setProntuario(prontuario);
+
+        professorRepository.save(professor);
+        System.out.println("Professor atualizado com sucesso !!!\n");
+
     }
 
 }
